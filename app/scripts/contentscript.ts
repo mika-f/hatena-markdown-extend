36348677fc07378445c82465df81330cbe3b0a81
Mikazuki <mikazuki_fuyuno@outlook.com>
2019-09-16 21:33:38 +0900

    feat: impl feature

--- app/scripts/contentscript.ts
+++ app/scripts/contentscript.ts
@@ -1,1 +1,6 @@
-console.log(`'Allo 'Allo! Content script`);
+import { MarkdownExtender } from "./features/MarkdownExtender";
+
+if (window.location.host === "blog.hatena.ne.jp") {
+  const editor = new MarkdownExtender("textarea.editor-body-textarea");
+  editor.connect();
+}
